dab5d4d8eeeea13be292d4aeed8296232d92853b
Stephanie <stephanie.zevian@yh.nackademin.se>
2024-03-20 11:15:07 +0100

    clean up

--- homepage/script.js
+++ homepage/script.js
@@ -15,3 +15,17 @@ function rotateImage() {
 function toggleSearchBar() {
     
 }
+
+
+const q = document.querySelectorAll('.q');
+const a = document.querySelectorAll('.a');
+const arr = document.querySelectorAll('.arrow');
+
+for(let i = 0; i < q.length; i++){
+    q[i].addEventListener('click', () => {
+
+        a[i].classList.toggle('a-opened');
+
+        arr[i].classList.toggle('arrow-rotaded');
+    });
+}
