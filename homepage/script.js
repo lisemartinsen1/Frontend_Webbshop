a009961ea46df52f96ccc63dc9c63f9b057d7103
Johanna Lundström <johanna.ml.lundstrom@gmail.com>
2024-03-25 15:45:15 +0100

    fixad fetch och local storage

--- homepage/script.js
+++ homepage/script.js
@@ -1,5 +1,3 @@
-
-
 let isRotated = false;
 
 function rotateImage() {
@@ -14,7 +12,7 @@ function rotateImage() {
 }
 
 function toggleSearchBar() {
-
+    
 }
 
 
@@ -22,7 +20,7 @@ const q = document.querySelectorAll('.q');
 const a = document.querySelectorAll('.a');
 const arr = document.querySelectorAll('.arrow');
 
-for (let i = 0; i < q.length; i++) {
+for(let i = 0; i < q.length; i++){
     q[i].addEventListener('click', () => {
 
         a[i].classList.toggle('a-opened');
@@ -32,170 +30,119 @@ for (let i = 0; i < q.length; i++) {
 }
 
 
-//fetch
-
-//dessa funkar inte som variabler.
-/*
-const categoryAll = `https://fakestoreapi.com/products?limit=${numCards}`;
-const categoryJewelery = `https://fakestoreapi.com/products/category/jewelery?limit=${numCards}`;
-const categoryWomen = `https://fakestoreapi.com/products/category/${womensClothing}?limit=${numCards}`;
-const categoryMen = `https://fakestoreapi.com/products/category/${mensClothing}?limit=${numCards}`;
-*/
-
-
-// fetchen ligger i en funktion, så inget laddas förrän man kallar på den yttersta funktionen i eventen.
-
 
 $(document).ready(function () {
+    addCards(`https://fakestoreapi.com/products`);
 
-    function fetchItems(path) {
+});
 
 
-        function createCard(id, image, title, description, price) {
-            return (
-                `<div class="col-6 col-sm-6 col-md-4 col-lg-3">
-                <div class="card h-100">
-                    <img src="${image}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title ">${title}</h5>
-                    </div>
-                    <div class="card-footer d-flex flex-column align-items-center">
-            
-                        <div class="accordion accordion-flush" id="accordionFlushExample">
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="flush-heading${id}">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#flush-collapse${id}" aria-expanded="false"
-                                        aria-controls="flush-collapse${id}">
-                                        Mer info
-                                    </button>
-                                </h2>
-            
-                                <div id="flush-collapse${id}" class="accordion-collapse collapse"
-                                    aria-labelledby="flush-heading${id}" data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">${description}</div>
-                                </div>
-                            </div>
+    // Funktion för att skapa ett "card" div-element med slumpmässig personinformation
+    function createCard(id, image, title, description, price) {
+        return `
+
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+        <div class="card h-100">
+            <img src="${image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title ">${title}</h5>
+            </div>
+            <div class="card-footer d-flex flex-column align-items-center">
+
+                <div class="accordion accordion-flush" id="accordionFlushExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-heading${id}">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#flush-collapse${id}" aria-expanded="false"
+                                aria-controls="flush-collapse${id}">
+                                Mer info
+                            </button>
+                        </h2>
+
+                        <div id="flush-collapse${id}" class="accordion-collapse collapse"
+                            aria-labelledby="flush-heading${id}" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body">${description}</div>
                         </div>
-                        <small class="text-muted">$${price}</small>
-                        <a class="btn order-btn btn-outline-dark my-2"href="../orderpage/order.html?id=${id}" role="button">Beställ</a>
                     </div>
                 </div>
-            </div>`
-            );
-        }
+                <small class="text-muted">$${price}</small>
+                <a class="btn btn-outline-dark my-2 order-btn" href="../orderpage/order.html" role="button">Beställ</a>
+                
+            </div>
+        </div>
+    </div>
+        `;
+    }
 
-        function addCards(numCards) {
-            const cardsGroup = $("#cardsRow");
-
+    
+    // Funktion för att lägga till fler "card" divar
+    function addCards(path) {
+        const cardsGroup = $("#cardsRow");
+        
             fetch(path)
                 .then(response => response.json())
                 .then(data => {
-                    for (let x = 0; x < numCards; x++) {
-                        const id = data[x].id;
-                        const img = data[x].image;
-                        const title = data[x].title;
-                        const description = data[x].description;
-                        const price = data[x].price;
+                    data.forEach(data => {
+                   
+                        const id = data.id;
+                        const img = data.image;
+                        const title = data.title;
+                        const description = data.description;
+                        const price = data.price;
                         // Skapa ett nytt "card" div-element och lägg till det i raden
                         const cardHTML = createCard(id, img, title, description, price);
                         cardsGroup.append(cardHTML);
-                    }
 
-                    $('.order-btn').click(function (event) {
-                        event.preventDefault();
-                    
-                        let card = $(this).closest('.card')
-                    
-                        let productImg = card.find('img.card-img-top').attr('src');
-                        let productTitle = card.find('h5.card-title').text();
-                        let productDescr = card.find('.accordion-body').text();
-                        let productPrice = card.find('small.text-muted').text();
-                    
-                        console.log(productTitle)
-                    
-                        localStorage.setItem('image', productImg);
-                        localStorage.setItem("title", productTitle);
-                        localStorage.setItem('description', productDescr);
-                        localStorage.setItem('price', productPrice);
-                    
-                        // FUNKAR!!!
-                        let getImg = localStorage.getItem('image');
-                        let getTitle = localStorage.getItem("title");
-                        let getDesc = localStorage.getItem('description');
-                        let getPrice = localStorage.getItem("price");
-                    
-                        console.log(getImg);
-                        console.log(getTitle);
-                        console.log(getDesc);
-                        console.log(getPrice);
-                        console.log(localStorage);
-                    
                     });
-                    
+
+                    $('.order-btn').click(function(event) {
+                     // event.preventDefault();
+
+                      let card = $(this).closest('.card')
+
+                      let productImg = card.find('img.card-img-top').attr('src');
+                      let productTitle = card.find('h5.card-title').text();
+                      let productDescr = card.find('.accordion-body').text();
+                      let productPrice = card.find('small.text-muted').text();
+
+                      localStorage.setItem('image', productImg);
+                      localStorage.setItem("title", productTitle);
+                      localStorage.setItem('description', productDescr);
+                      localStorage.setItem('price', productPrice);
+
+                      // FUNKAR!!!
+                      let getImg = localStorage.getItem('image');
+                      let getTitle = localStorage.getItem("title");
+                      let getDesc = localStorage.getItem('description');
+                      let getPrice = localStorage.getItem("price");
+                      
+                      console.log(getImg);
+                      console.log(getTitle);
+                      console.log(getDesc);
+                      console.log(getPrice);
+                      
+                  });
 
                 })
                 .catch(error => console.error("Error fetching random product:", error));
-        }
+    }
 
-        // Anropa funktionen för att lägga till fler "card" divar
-        addCards(20);
-
-    } //fetchItems
-
-        //remove child elements before fetch new products
-        const parent = document.getElementById('cardsRow');
-        function removeAllChildNodes(parent) {
-            while (parent.firstChild) {
-                parent.removeChild(parent.firstChild);
-            }
-        }
-    
-        //Hämtar elementene som ska få event listeners
-        const womensCat = document.getElementById('womenCategory');
-        const mensCat = document.getElementById('menCategory');
-        const jeweleryCat = document.getElementById('jeweleryCategory');
-        const allCategories = document.getElementById('allCategories');
-    
-        // variabler till fetch-url
+    $('#womenCategory').click(function () {
+       
         const womensClothing = encodeURIComponent("women's clothing");
+        $("#cardsRow").empty(); // Clear existing cards
+        addCards(`https://fakestoreapi.com/products/category/${womensClothing}`);
+    });
+
+    $('#mensCategory').click(function () {
+       
         const mensClothing = encodeURIComponent("men's clothing");
-    
-    
-        //event listeners för kategorierna
-        womensCat.addEventListener('click', fetchWomens);
-        const numCards = 20;
-    
-        function fetchWomens() {
-            console.log('womens category');
-            removeAllChildNodes(parent);
-            fetchItems(`https://fakestoreapi.com/products/category/${womensClothing}?limit=${numCards}`);
-        }
-    
-        mensCat.addEventListener('click', fetchMens);
-    
-        function fetchMens() {
-            console.log('mens category');
-            removeAllChildNodes(parent);
-            fetchItems(`https://fakestoreapi.com/products/category/${mensClothing}?limit=${numCards}`);
-        }
-    
-        jeweleryCat.addEventListener('click', fetchJewelary);
-    
-        function fetchJewelary() {
-            console.log('jewelery category');
-            removeAllChildNodes(parent);
-            fetchItems('https://fakestoreapi.com/products/category/jewelery?limit=${numCards}')
-        }
-    
-        allCategories.addEventListener('click', fetchAll);
-    
-        function fetchAll() {
-            console.log('all categories');
-            removeAllChildNodes(parent);
-            fetchItems('https://fakestoreapi.com/products?limit=${numCards}');
-        }
+        $("#cardsRow").empty(); // Clear existing cards
+        addCards(`https://fakestoreapi.com/products/category/${mensClothing}`);
+    });
 
-    fetchAll();
-
-}); //document.ready
+    $('#jeweleryCategory').click(function () {
+    
+        $("#cardsRow").empty(); // Clear existing cards
+        addCards(`https://fakestoreapi.com/products/category/jewelery`);
+    });
