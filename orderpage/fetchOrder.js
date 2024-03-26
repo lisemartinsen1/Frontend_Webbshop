f1cd743ed9015c7b81fc525890e7dc080f1d7992
Lise Martinsen <lise.martinsen@yh.nackademin.se>
2024-03-26 11:05:07 +0100

    Ny javascript-fil (fetchOrder.js) för att hämta produktinfo och skapa upp card.

---
+++ orderpage/fetchOrder.js
@@ -0,0 +1,47 @@
+const params = new URLSearchParams(window.location.search);
+const id = params.get('id');
+console.log(id);
+
+//error här
+$(document).ready(function () {
+
+    const cardInput = $('#cardInput');
+
+    fetch(`https://fakestoreapi.com/products/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            
+
+                const id = data.id;
+                const img = data.image;
+                const title = data.title;
+                const description = data.description;
+                const price = data.price;
+                // Skapa ett nytt "card" div-element och lägg till det i raden
+                const cardHTML = createCard(id, img, title, description, price);
+                cardInput.append(cardHTML);
+
+        
+
+        })
+        .catch(error => console.error("Error fetching random product:", error));
+
+});
+
+function createCard(id, image, title, description, price) {
+    return `
+
+                <div class="col-12">
+                    <img src="${image}" alt="product image">
+                </div>
+                <div class="col-12">
+                    <h3>${title}</h3>
+                </div>
+                <div class="col-12">
+                    <p>${description}</p>
+                </div>
+                <div class="col-12">
+                    <h5>${price}</h5>
+                </div>
+`;
+}
